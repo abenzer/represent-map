7f0e6eb08906c6844f169031c466835b35c12e04
Alex <Alex@192.168.1.102>
2012-06-07 01:57:50 -0700

    Added db structure

---
+++ represent-map/db/places_1.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE IF NOT EXISTS `places` (
+  `id` int(9) NOT NULL AUTO_INCREMENT,
+  `approved` int(1) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `lat` float NOT NULL,
+  `lng` float NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `uri` varchar(200) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `sector` varchar(50) NOT NULL,
+  `owner_name` varchar(100) NOT NULL,
+  `owner_email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=777 ;
