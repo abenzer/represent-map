d5333853d8d1f0bffb1b7da78ca4892bcf4ab73e
Alex <Alex@192.168.1.102>
2012-10-08 23:14:17 -0700

    Fixed a bug with parseInput() in events_get.php

--- represent-map/header.php
+++ represent-map/header.php
@@ -5,13 +5,5 @@ include "./include/db.php";
 mysql_connect($db_host, $db_user, $db_pass) or die(mysql_error());
 mysql_select_db($db_name) or die(mysql_error());
 
-// useful functions
-function parseInput($value) {
-  $value = htmlspecialchars($value);
-  $value = str_replace("\r", "", $value);
-  $value = str_replace("\n", "", $value);
-  return $value;
-}
-
 
 ?>
