173446a1e3a743b36d9caca54ef1a5d2dbc2deb8
Alexandre Gherschon <alexandre.gherschon@gmail.com>
2012-12-23 10:49:23 +0100

    issue #20 add json output for places, by type

---
+++ represent-map/json.php
@@ -0,0 +1,33 @@
+<?php
+  include_once "header.php";
+
+  $types = Array(
+    Array('startup', 'Startups'),
+    Array('accelerator','Accelerators'),
+    Array('incubator', 'Incubators'),
+    Array('coworking', 'Coworking'),
+    Array('investor', 'Investors'),
+    Array('service', 'Consulting'),
+    Array('hackerspace', 'Hackerspaces'),
+    Array('event', 'Events'),
+  );
+  $marker_id = 0;
+  $markers = Array();
+  foreach($types as $type) {
+    $places = mysql_query("SELECT * FROM places WHERE approved='1' AND type='$type[0]' ORDER BY title");
+    $places_total = mysql_num_rows($places);
+    while($place = mysql_fetch_assoc($places)) {
+      $place[title] = htmlspecialchars_decode(addslashes(htmlspecialchars($place[title])));
+      $place[description] = htmlspecialchars_decode(addslashes(htmlspecialchars($place[description])));
+      $place[uri] = addslashes(htmlspecialchars($place[uri]));
+      $place[address] = htmlspecialchars_decode(addslashes(htmlspecialchars($place[address])));
+      $markers[$type[0]][$marker_id] = $place;
+      $marker_id++;
+    }
+  }
+
+  //var_dump($markers);
+  //echo "<br><br>";
+  //$to_json = json_encode($markers);
+  echo(json_encode($markers));
+?>
